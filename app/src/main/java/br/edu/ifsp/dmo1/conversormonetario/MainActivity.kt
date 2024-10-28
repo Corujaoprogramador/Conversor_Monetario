b4f48382601a91fb3b0649613f8ad3dca851900f
Corujaoprogramador <deivid9pereira@gmail.com>
2024-10-28 00:41:40 -0300

    Melhorias de funcionamento e realização de duas conversões

--- app/src/main/java/br/edu/ifsp/dmo1/conversormonetario/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/conversormonetario/MainActivity.kt
@@ -11,7 +11,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 private val DOLLAR_VALUE = 5.50
 
     private lateinit var inputEditText: EditText
-    private lateinit var calculateButton: Button
+    private lateinit var toDollarButton: Button
+    private lateinit var toRealButton: Button
     private lateinit var outputTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,28 +23,41 @@ private val DOLLAR_VALUE = 5.50
     }
 
     override fun onClick(v: View) {
-        if (v == calculateButton) {
-            makeConvertion()
+        if (v == toDollarButton) {
+            dollarConversion()
+        }else if (v == toRealButton){
+            realConversion()
         }
     }
 
     private fun findById() {
         inputEditText = findViewById(R.id.edittext_value)
-        calculateButton = findViewById(R.id.button_calculate)
+        toDollarButton = findViewById(R.id.button_convert_to_dollar)
+        toRealButton = findViewById(R.id.button_convert_to_real)
         outputTextView = findViewById(R.id.textview_output)
     }
 
     private fun configClickListener() {
-        calculateButton.setOnClickListener(this)
+        toRealButton.setOnClickListener(this)
+        toDollarButton.setOnClickListener(this)
     }
 
-    private fun makeConvertion() {
-        var value = try {
+    private fun getValue(): Double{
+        return try {
             inputEditText.text.toString().toDouble()
         } catch (e: NumberFormatException) {
             0.0
         }
+    }
+    private fun dollarConversion() {
+        var value = getValue()
         value = value / DOLLAR_VALUE
         outputTextView.text = "U$ $value"
     }
+    private fun realConversion(){
+        var value = getValue()
+        value = value * DOLLAR_VALUE
+        outputTextView.text = "R$ $value"
+    }
 }
+
